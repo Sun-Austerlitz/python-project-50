af31b4f22f1cc14c46d7d61ce7fc2a0fed654cea
Sun-Austerlitz <sankibaev.asqar@gmail.com>
2025-01-28 01:21:51 +0500

    Step 2: Arguments and options

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,11 +3,18 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference.")
+        description="Compares two configuration files and shows a difference."
+    )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument(
+        "-f",
+        "--format",
+        help="set format of output",
+        dest="format",
+    )
     parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
