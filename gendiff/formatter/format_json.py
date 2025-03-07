d2d76dbe752840dd11becaf71140bd5f83b39ea0
Sun-Austerlitz <sankibaev.asqar@gmail.com>
2025-03-07 22:34:12 +0500

    Refactoring: rename cli.py to utils.py, update formatter.py and format_json.py

--- gendiff/formatter/format_json.py
+++ gendiff/formatter/format_json.py
@@ -1,28 +1,9 @@
 import json
-from typing import Any, Dict, List
 
 
-def format_json(diff: List[Dict[str, Any]]) -> str:
-    """
-    Formats the diff in a JSON format.
+def format_json(diff):
 
-    Parameters:
-        diff (list): The diff representation.
-
-    Returns:
-        str: The formatted JSON diff.
-    """
-
-    def convert_to_nested_dict(diff: List[Dict[str, Any]]) -> Dict[str, Any]:
-        """
-        Converts the diff list to a nested dictionary format.
-
-        Parameters:
-            diff (list): The diff representation.
-
-        Returns:
-            dict: The nested dictionary representation.
-        """
+    def convert_to_nested_dict(diff):
         nested_dict = {}
         for item in diff:
             key = item["key"]
--- gendiff/formatter_selector.py
+++ gendiff/formatter_selector.py
@@ -1,25 +1,27 @@
-from typing import Any
+from typing import Any, Dict, List
 
 from .formatter.format_json import format_json
 from .formatter.format_plain import format_plain
 from .formatter.format_stylish import format_stylish
 
 
-def select_formatter(format_name: str) -> Any:
+def select_formatter(diff: List[Dict[str, Any]], format_name: str) -> str:
     """
-    Selects the appropriate formatter based on the format name.
+    Selects the appropriate formatter
+    based on the format name and returns the formatted diff.
 
     Parameters:
+        diff (list): The diff representation.
         format_name (str): The format type for the output.
 
     Returns:
-        function: The selected formatter function.
+        str: The formatted diff.
     """
     if format_name == "stylish":
-        return format_stylish
+        return format_stylish(diff)
     elif format_name == "plain":
-        return format_plain
+        return format_plain(diff)
     elif format_name == "json":
-        return format_json
+        return format_json(diff)
     else:
         raise ValueError(f"Unsupported format: {format_name}")
